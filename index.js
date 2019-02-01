2e68244c47b894c2f50e8034e0838692209dc745
Imelda Izaguirre <imeldaizaguirre@gmail.com>
2019-02-01 05:14:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,12 +62,14 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === cardNumber) {
-      cart.splice(0,cart.length);
-      return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}`
+  if (cardNumber !== cardNumber) {
 
-  }   else {
-      return "Sorry, we don't have a credit card on file for you.";
+    return "Sorry, we don't have a credit card on file for you.";
+
+  } else {
+    cart.splice(0,cart.length);
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}`
+
   }
 
 }
