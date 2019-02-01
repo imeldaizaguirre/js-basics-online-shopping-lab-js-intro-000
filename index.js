357ed335b8a06f0e433ce9a9f62a1ab2d47e1d85
Imelda Izaguirre <imeldaizaguirre@gmail.com>
2019-02-01 05:10:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,7 +63,6 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber === cardNumber) {
-      cardNumber.parseInt(cardNumber);
       cart.splice(0,cart.length);
       return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}`
 
