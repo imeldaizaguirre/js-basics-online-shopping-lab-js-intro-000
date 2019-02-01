ec5865913f07ce381a3332e4387020c7226602f4
Imelda Izaguirre <imeldaizaguirre@gmail.com>
2019-02-01 05:09:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,10 +63,12 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber === cardNumber) {
-    cardNumber.parseInt(cardNumber);
-    cart.splice(0,cart.length);
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}`
+      cardNumber.parseInt(cardNumber);
+      cart.splice(0,cart.length);
+      return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}`
 
+  }   else {
+      return "Sorry, we don't have a credit card on file for you.";
   }
 
 }
